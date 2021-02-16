9e88af58a2f791439f6b49950854508e9ec69640
Christophe Lecroq <christophe.lecroq@gmail.com>
2021-02-16 13:43:13 +0100

    feat(*): initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// in src/App.js
+import * as React from "react";
+import {Admin, Resource} from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import {UserList} from "./users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://127.0.0.1:8001/api');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="costumes" list={UserList}/>
+    </Admin>
+);
 
 export default App;
